c070ff298b0f6d8121118c912928565289fb089d
Wooram Jung <jungwooram@Wooram-MacBookPro.local>
2020-10-28 22:05:05 +0100

    fisrt commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Wooram Jung on 2020-10-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
